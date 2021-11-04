a56b57c14a7092ba633c8cf279776f48a61275eb
Edrian-Jose <edrianjoseferrer@gmail.com>
2021-11-04 17:09:39 +0800

    Create StorageSystem

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    [SerializeField]
+    private EntityStats stats;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
